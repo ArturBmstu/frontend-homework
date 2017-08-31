161df7a7990e1d7061e41065689bf9fc41b7c11f
ArturBmstu <artur050711@gmail.com>
2017-08-31 19:36:21 +0400

    zip

---
+++ source/zip.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const zip = function(...args) {
+	let dst = {};
+	args.forEach(function(arg) {
+		for (let key in arg) {
+			if !(key in dst)
+				dst[key] = arg[key];
+		}
+	})
+	return dst;
+}
