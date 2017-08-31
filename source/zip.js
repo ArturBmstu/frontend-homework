42ff701089cebfb2d58c2d4862aca015a9dfc8e5
ArturBmstu <artur050711@gmail.com>
2017-08-31 23:51:08 +0400

    use spread

--- source/zip.js
+++ source/zip.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const zip = (...args) => args.reduce(function(dst, arg) {
-		for (let key in arg) {
-			if (!(key in dst))
-				dst[key] = arg[key];
-		}
-		return dst;
-	})
+const zip = (...args) => args.reduce(
+	(dst, arg) =>  ({...arg, ...dst})
+);
