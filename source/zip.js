419bad03846b4ad60e8fdff4e5cf7e3c07485d82
ArturBmstu <artur050711@gmail.com>
2017-08-31 23:33:38 +0400

    use arrow function

--- source/zip.js
+++ source/zip.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const zip = function(...args) {
-	return args.reduce(function(dst, arg) {
+const zip = (...args) => args.reduce(function(dst, arg) {
 		for (let key in arg) {
 			if (!(key in dst))
 				dst[key] = arg[key];
 		}
 		return dst;
 	})
-}
